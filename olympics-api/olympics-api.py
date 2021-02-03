712b499351f7a0d9d1bd7ad45a5a9f0dd4f5ee1a
Kyosuke Imai <kyosuke912@eduroam-165-244.dyn.carleton.edu>
2021-02-03 02:20:31 -0600

    edit comments

--- olympics-api/olympics-api.py
+++ olympics-api/olympics-api.py
@@ -21,7 +21,7 @@ def connect_to_database():
         exit()
 
 def sort_query(query,connection):
-    """"""
+    """make cursor object for data sorted based on query"""
     try:
         cursor = connection.cursor()
         this_query = query
@@ -31,10 +31,12 @@ def sort_query(query,connection):
         print(e)
         exit()
 
-"""method that makes game list of dictionaries
-{'id': id, 'year': year, 'season': season, 'city': city}
-"""
+
 def make_games_dictionaries(connection):
+    """method that makes list of dictionaries for olympic games
+    {'id': id, 'year': year, 'season': season, 'city': city}
+    returns list of dictionaries
+    """
     query = """SELECT * FROM olympic_games
     ORDER BY year DESC;"""
     result_list = []
@@ -43,9 +45,11 @@ def make_games_dictionaries(connection):
         result_list.append({'id': row[0], 'year': row[1], 'season': row[2], 'city': row[3]})
     return result_list
 
-"""method that makes nocs list of dictionaries
-{'abbreviation': abbreviation, 'name': name}"""
+
 def make_noc_dictionaries(connection):
+    """method that makes list of dictionaries for noc
+    {'abbreviation': abbreviation, 'name': name}
+    returns list of dictionaries"""
     query = """SELECT DISTINCT noc, noc_full
     FROM nocs, noc_regions
     WHERE noc_regions.noc_abbreviation = nocs.noc
@@ -56,10 +60,11 @@ def make_noc_dictionaries(connection):
         result_list.append({'abbreviation': row[0], 'name': row[1]})
     return result_list
 
-"""method that makes athlete list of dictionaries
-{'athlete_id': athlete_id, 'athlete_name': athlete_name, 'athlete_sex':athlete_sex, 'sport':sport, 'event':event, 'medal':medal
-}"""
+
 def make_athlete_dictionaries(connection, oly_game_id):
+    """method that makes list of dictionaries for athletes
+    {'athlete_id': athlete_id, 'athlete_name': athlete_name, 'athlete_sex':athlete_sex, 'sport':sport, 'event':event, 'medal':medal
+    }"""
     query = """SELECT DISTINCT athletes.athlete_ID, athlete_name, sex, sport_category, detailed_event, medal
     FROM athletes, main_events, olympic_games, detailed_events, medals, sport_categories
     WHERE olympic_games.oly_game_ID = """ + oly_game_id + """
@@ -77,7 +82,7 @@ def make_athlete_dictionaries(connection, oly_game_id):
         result_list.append({'athlete_id': row[0], 'athlete_name': row[1], 'athlete_sex': row[2], 'sport': row[3], 'event': row[4], 'medal': row[5]})
     return result_list
 
-"""Creating the athlete dictionary for methods below"""
+"""Creating the athlete list of dictionaries for methods below"""
 def make_athlete_dictionaries_with_nocs(connection, oly_game_id, nocs):
     query = """SELECT DISTINCT athletes.athlete_ID, athlete_name, sex, sport_category, detailed_event, medal
     FROM athletes, main_events, olympic_games, detailed_events, medals, sport_categories, nocs
@@ -91,32 +96,29 @@ def make_athlete_dictionaries_with_nocs(connection, oly_game_id, nocs):
     AND nocs.nocs_id = main_events.noc_id
     AND (medal = 'Gold' OR medal = 'Silver' OR medal = 'Bronze'  )
     ; """
-"""Create a result_list list to store through querey"""
+
     result_list = []
     cursor = sort_query(query, connection)
     for row in cursor:
         result_list.append({'athlete_id': row[0], 'athlete_name': row[1], 'athlete_sex': row[2], 'sport': row[3], 'event': row[4], 'medal': row[5]})
     return result_list
 
-"""The greeting "page for users"""
 @app.route('/')
 def hello():
     return 'Hello, Welcome to our Olympics database!'
 
-"""Returns all olympic games"""
 @app.route('/games')
 def get_games():
-    """list of games dictionary sorted by year"""
+    """returns json list of dictionaries of olympics games attributes"""
     games_list = []
     connection = connect_to_database()
     for game in make_games_dictionaries(connection):
         games_list.append(game)
     return json.dumps(games_list)
 
-"""Returns all olympic NOCS"""
 @app.route('/nocs')
 def get_nocs():
-    """list of nocs dictionary alphabetized by NOC abbreviation"""
+    """returns json list of dictionaries of noc attributes""""
     nocs_list = []
     connection = connect_to_database()
     for noc in make_noc_dictionaries(connection):
@@ -124,26 +126,22 @@ def get_nocs():
     return json.dumps(nocs_list)
 
 
-"""Returns all medalists who won a medal at a center olympic game given by the user via HTML"""
 @app.route('/medalists/games/<games_id>')
 def get_medalists_noc(games_id):
-    """List of medalist dictionaries of medalists in specified olympic games"""
+    """returns json list of dictionaries depending on argument"""
     medalist_list = []
     oly_game_id = games_id
     connection = connect_to_database()
     noc = flask.request.args.get('noc')
     string_noc = str(noc)
-    
-    """Optional parameter, if the user decides to enter a NOC, return all olympians who won a medal from that NOC at that olympic game"""
+
     if noc:
         for medalist in make_athlete_dictionaries_with_nocs(connection, oly_game_id, string_noc):
             medalist_list.append(medalist)
-    """If the user decides does not enter a NOC, return all olympians who won a medal at that olympic game"""
     else:
         for medalist in make_athlete_dictionaries(connection, oly_game_id):
             medalist_list.append(medalist)
 
-    """Returns a list of medal winners for a game or for a certain NOC at that gamex"""
     return json.dumps(medalist_list)
 
 
