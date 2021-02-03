6aec879e39bb4d5c45c8a5f5fd9b4dc71fb6b195
Kyosuke Imai <kyosuke912@eduroam-165-244.dyn.carleton.edu>
2021-02-02 22:03:26 -0600

    updated olympics-api

--- olympics-api/olympics-api.py
+++ olympics-api/olympics-api.py
@@ -3,16 +3,9 @@ import argparse
 import psycopg2
 import json
 import flask
+import sys
 
 app = flask.Flask(__name__)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser('')
-    parser.add_argument('host', help='the host on which this application is running')
-    parser.add_argument('port', type=int, help='the port on which this application is listening')
-    arguments = parser.parse_args()
-    app.run(host=arguments.host, port=arguments.port, debug=True)
-
 
 def connect_to_database():
     from config import password
@@ -43,9 +36,9 @@ def make_games_dictionaries(connection):
     query = """SELECT * FROM olympic_games
     ORDER BY year DESC;"""
     result_list = []
-    cursor = sort_by_query(query, connection)
+    cursor = sort_query(query, connection)
     for row in cursor:
-        result_list.append[{'id': row[0], 'year': row[1], 'season': row[2], 'city': row[3]}]
+        result_list.append({'id': row[0], 'year': row[1], 'season': row[2], 'city': row[3]})
     return result_list
 
 """method that makes nocs list of dictionaries
@@ -56,17 +49,17 @@ def make_noc_dictionaries(connection):
     WHERE noc_regions.noc_abbreviation = nocs.noc
     ORDER BY noc;"""
     result_list = []
-    cursor = sort_by_query(query, connection)
+    cursor = sort_query(query, connection)
     for row in cursor:
-        result_list.append[{'abbreviation': row[0], 'name': row[1]}]
+        result_list.append({'abbreviation': row[0], 'name': row[1]})
     return result_list
 
 """method that makes athlete list of dictionaries
 {'athlete_id': athlete_id, 'athlete_name': athlete_name, 'athlete_sex':athlete_sex, 'sport':sport, 'event':event, 'medal':medal
 }"""
 def make_athlete_dictionaries(connection, oly_game_id):
-    query = """SELECT athlete_id, athlete_name, sex, sport_categories.wport_category, detailed_event, medal
-    FROM athletes, main_events, detailed_events, medals, olympic_games
+    query = """SELECT athletes.athlete_id, athlete_name, sex, sport_categories.sport_category, detailed_event, medal
+    FROM athletes, main_events, detailed_events, medals, olympic_games, sport_categories
     WHERE athletes.athlete_id = main_events.athlete_id
     AND main_events.oly_game_id = """ + oly_game_id + """
     AND medals.medal_id = main_events.medal_ID
@@ -77,17 +70,33 @@ def make_athlete_dictionaries(connection, oly_game_id):
     """SELECT athletes.athlete_id, athlete_name, sex, sport_categories.sport_category, detailed_event, medal
     FROM athletes, main_events, detailed_events, medals, olympic_games, sport_categories
     WHERE athletes.athlete_id = main_events.athlete_id
-    AND main_events.oly_game_id = 2
     AND medals.medal_id = main_events.medal_ID
     AND detailed_events.detailed_event_id = main_events.detailed_event_id
-    AND sport_categories.sport_category_id = main_events.sport_category_id;"""
+    AND sport_categories.sport_category_id = main_events.sport_category_id;
+    """
+
+    # query = """SELECT athletes.athlete_id, athlete_name, sex, sport_categories.sport_category, detailed_event, medal
+    # FROM athletes, main_events, detailed_events, medals, olympic_games, sport_categories
+    # WHERE athletes.athlete_id = main_events.athlete_id
+    # AND medals.medal_id = main_events.medal_id
+    # AND detailed_events.detailed_event_id = main_events.detailed_event_id
+    # AND sport_categories.sport_category_id = main_events.sport_category_id;"
+    #
+    # WHERE main_events.oly_game_id = 2
+    # AND athletes.athlete_id = main_events.athlete_id
+    # AND medals.medal_id = main_events.medal_id
+    # AND detailed_events.detailed_event_id = main_events.detailed_event_id
+    # AND sport_categories.sport_category_id = main_events.sport_category_id;"""
 
     result_list = []
-    cursor = sort_by_query(query, connection)
+    cursor = sort_query(query, connection)
     for row in cursor:
-        result_list.append[{'athlete_id': row[0], 'athlete_name': row[1], 'athlete_sex': row[2], 'sport': row[3], 'event': row[4], 'medal': row[5]}]
+        result_list.append({'athlete_id': row[0], 'athlete_name': row[1], 'athlete_sex': row[2], 'sport': row[3], 'event': row[4], 'medal': row[5]})
     return result_list
 
+@app.route('/')
+def hello():
+    return 'Hello, Citizen of CS257.'
 
 @app.route('/games')
 def get_games():
@@ -108,13 +117,20 @@ def get_nocs():
     return json.dumps(nocs_list)
 
 @app.route('/medalists/games/<games_id>')
-def get_medalists():
+def get_medalists(games_id):
     """list of medalist dictionaries of medalists in specified olympic games"""
     medalist_list = []
     connection = connect_to_database()
     noc = flask.request.args.get('noc')
-    for medalist in make_athlete_dictionaries(connection):
-        if noc is not None and noc != make_athlete_dictionaries(connection)['noc']:
+    for medalist in make_athlete_dictionaries(connection, games_id):
+        if noc is not None and noc != make_athlete_dictionaries(connection, games_id)['noc']:
             continue
         medalist_list.append(medalist)
     return json.dumps(medalist_list)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('')
+    parser.add_argument('host', help='the host on which this application is running')
+    parser.add_argument('port', type=int, help='the port on which this application is listening')
+    arguments = parser.parse_args()
+    app.run(host=arguments.host, port=arguments.port, debug=True)
